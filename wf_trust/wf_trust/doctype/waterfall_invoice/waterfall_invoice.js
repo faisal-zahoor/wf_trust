12e53bdcd154d8cc08efede0b82f3862163444e1
Faisal Zahoor Vedani <faisal-zahoor_vedani>
2025-03-29 13:44:25 -0400

    feat: doctypes

---
+++ wf_trust/wf_trust/doctype/waterfall_invoice/waterfall_invoice.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, NexTash (SMC-PVT) Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Waterfall Invoice", {
+// 	refresh(frm) {
+
+// 	},
+// });

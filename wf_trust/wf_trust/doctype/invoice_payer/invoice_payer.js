5ada21792214266ca4ee2e97f15ae1ebaec2543d
Raja Abdullah Arshad <rajaabdullaharsahd>
2025-03-29 23:42:36 +0500

    set queries

--- wf_trust/wf_trust/doctype/invoice_payer/invoice_payer.js
+++ wf_trust/wf_trust/doctype/invoice_payer/invoice_payer.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2025, NexTash (SMC-PVT) Ltd and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Invoice Payer", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Invoice Payer", {
+	setup(frm) {
+        frm.set_query("account", function() {
+            return {
+                filters: {
+                    company: frm.doc.company
+                }
+            };
+        });
+	},
+});
